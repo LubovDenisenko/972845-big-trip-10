6b1587070095c9d703400d86db042e22fe298b9e
Lubov <denisenkolubov@gmail.com>
2019-11-25 10:15:11 +0300

    done

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,8 @@ module.exports = {
   devtool: `source-map`,
   devServer: {
     contentBase: path.join(__dirname, `public`),
-    publicPath: 'http://localhost:8080/',\compress: true,
+    publicPath: 'http://localhost:8080/',
+    compress: true,
     watchContentBase: true
   }
 };
